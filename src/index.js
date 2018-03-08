78ce9ed0db0eaacb88d08c8234cb53409a1517e6
Les Moffat <les@lesmoffat.co.uk>
2018-03-08 21:44:51 +0000

    export consts correctly

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 import SSRSwitch from './ssr-switch'
 
-const ClientOnly = props => <SSRSwitch env="client" {...props} />
-const ServerOnly = props => <SSRSwitch env="server" {...props} />
+export const ClientOnly = props => <SSRSwitch env="client" {...props} />
+export const ServerOnly = props => <SSRSwitch env="server" {...props} />
 
 export default SSRSwitch
-export {
-  ClientOnly,
-  ServerOnly,
-}
+
